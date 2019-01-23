61a6bc47866b1ebc15394419489c57d6951a20e3
awolak <awolakk@gmail.com>
2019-01-23 16:35:13 +0100

    zad03 update

--- zad03/db/runHSQLDBClient.bat
+++ zad03/db/runHSQLDBClient.bat
@@ -1,3 +1,1 @@
-#!/bin/sh
-
 java -cp hsqldb.jar  org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
--- zad03/src/main/java/ug/awolak/techut/zad03/Main.java
+++ zad03/src/main/java/ug/awolak/techut/zad03/Main.java
@@ -13,20 +13,20 @@ public class Main {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws SQLException, ParseException {
 
-			Bed b1 = new Bed("Bed1", Double.parseDouble("200.85"), new Date(118, 12, 30), true);
-	    	Bed b2 = new Bed("Bed2", Double.parseDouble("450.90"), new Date(115, 12, 30), false);
+			Bed b1 = new Bed("Bed1", Double.parseDouble("200.85"), new Date(2018, 12, 18), true);
+	    	Bed b2 = new Bed("Bed2", Double.parseDouble("450.90"), new Date(2018, 12, 30), false);
 	    	
-	        BedService ws = new BedService();
+	        BedService bs = new BedService();
 	
-	        ws.addBed(b1);
-	        ws.addBed(b2);
+	        bs.addBed(b1);
+	        bs.addBed(b2);
 	        
 	        List<Bed> beds = new ArrayList<Bed>();
 	        beds.add(b1);
 	        beds.add(b2);
-	        ws.addBeds(beds);
+	        bs.addBeds(beds);
 	        
-	        Bed bed = ws.getBedById(2);
+	        Bed bed = bs.getBedById(2);
 	}
 
 }
